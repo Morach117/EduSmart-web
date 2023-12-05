9c16550d846b4aa32f8e7f10aedb402ae03f48a5
Morach117 <morach117@gmail.com>
2023-12-05 09:14:22 -0600

    correciones con el modal de equipos

--- pages/WorkTeams.php
+++ pages/WorkTeams.php
@@ -1,56 +1,62 @@
-    <div class="page-body">
-        <div class="container-xl">
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-header">
-                        <div class="page-tittle col">
-                            <h2 class="page-tittle-text">Lista de alumnos</h2>
-                        </div>
-                        <div class="text-end col">
-                            <button data-bs-toggle="modal" data-bs-target="#modal-equipos"
-                                class="btn btn-primary">Administrar equipos</button>
-                        </div>
+<div class="page-body">
+    <div class="container-xl">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-header">
+                    <div class="page-tittle col">
+                        <h2 class="page-tittle-text">Lista de alumnos</h2>
                     </div>
+                    <div class="text-end col">
+                        <button data-bs-toggle="modal" data-bs-target="#modal-equipos" class="btn btn-primary">Administrar equipos</button>
+                    </div>
+                </div>
 
-                    <div class="table-responsive">
-                        <table class="table table-striped table-hover" id="example">
-                            <thead>
-                                <tr>
-                                    <th>Equipo</th>
-                                    <th>Integrantes</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover" id="example">
+                        <thead>
+                            <tr>
+                                <th>Equipo</th>
+                                <th>Integrantes</th>
+                                <th>Acciones</th> <!-- Agregamos una nueva columna para el botón de acción -->
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $selEquipos = $conn->query("SELECT * FROM equipos ORDER BY id_equipo");
+                            if ($selEquipos->rowCount() > 0) {
+                                while ($selEquipoRow = $selEquipos->fetch(PDO::FETCH_ASSOC)) {
+                                    ?>
+                                    <tr>
+                                        <td>
+                                            <?php echo $selEquipoRow['nombre_equipo']; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $selEquipoRow['numero_integrantes']; ?>
+                                        </td>
+                                        <td>
+                                            <!-- Agregamos un botón que activa el modal interno -->
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#integrantesModal" data-id-equipo="<?php echo $selEquipoRow['id_equipo']; ?>">
+                                                Ver Integrantes
+                                            </button>
+                                        </td>
+                                    </tr>
                                 <?php
-                                $selEquipos = $conn->query("SELECT * FROM equipos ORDER BY id_equipo");
-                                if ($selEquipos->rowCount() > 0) {
-                                    while ($selEquipoRow = $selEquipos->fetch(PDO::FETCH_ASSOC)) {
-                                        ?>
-                                        <tr data-toggle="modal" data-target="#integrantesModal" data-id-equipo="<?php echo $selEquipoRow['id_equipo']; ?>">
-                                            <td>
-                                                <?php echo $selEquipoRow['nombre_equipo']; ?>
-                                            </td>
-                                            <td>
-                                                <?php echo $selEquipoRow['numero_integrantes']; ?>
-                                            </td>
-                                        </tr>
-                                    <?php
-                                    }
-                                } else {
-                                    echo "<tr><td colspan='2'>No hay equipos registrados.</td></tr>";
                                 }
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
+                            } else {
+                                echo "<tr><td colspan='3'>No hay equipos registrados.</td></tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
+    </div>
 
     <!-- Script para manejar la apertura del modal -->
     <script>
-        // Reemplaza el evento de clic en la fila para abrir el modal
-        $('table').on('click', 'tr', function () {
+        // Reemplaza el evento de clic en el botón para abrir el modal
+        $('table').on('click', 'button', function () {
             var idEquipo = $(this).data('id-equipo');
 
             $.ajax({
@@ -67,6 +73,10 @@
             });
         });
 
+        // Agrega un evento para mostrar el modal interno al cargar la página
+        $(document).ready(function () {
+            $('#modal-interno').modal('show');
+        });
     </script>
 
     <!-- Script para manejar el botón de submit -->
@@ -113,5 +123,4 @@
         });
     </script>
 </body>
-
 </html>
